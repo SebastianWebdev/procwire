29892deb4423d324ac4ae5d55b6ee7f15ca2b092
Sebastian <sebastian.golebiowski@tensquaregames.com>
2026-01-29 14:32:57 +0100

    delete deprecated packages

--- packages/transport/src/resilience/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Resilience module combining heartbeat, reconnect, and shutdown.
- *
- * @packageDocumentation
- * @module Resilience
- */
-
-export * from "./types.js";
-export * from "./handle.js";
